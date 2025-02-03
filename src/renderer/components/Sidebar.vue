6299280d2cd57e6a2234da7ed7e5b1516a8120bc
Platina <kkgordon.com@gmail.com>
2025-02-03 18:35:57 +0900

    사이드바에서 로그인할 때 양옆을 눌러서 로그인 창을 끌 수 없는 문제 해결

--- src/renderer/components/Sidebar.vue
+++ src/renderer/components/Sidebar.vue
@@ -112,12 +112,11 @@ onMounted(() => {
     </v-list>
   </v-navigation-drawer>
 
-  <v-dialog v-model="bIsSignInModalOpened">
+  <v-dialog v-model="bIsSignInModalOpened" max-width="448">
     <v-card
         title="Bitmap ID"
         class="mx-auto pa-12 pb-8"
         elevation="8"
-        max-width="448"
         rounded="lg">
       <div class="text-subtitle-1 text-medium-emphasis">{{ t('id') }}</div>
       <v-text-field
